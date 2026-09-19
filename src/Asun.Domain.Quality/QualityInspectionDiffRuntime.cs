5a6d288a62aaf3b9fd3797ea00c4afc3107c57fb
asun01 <254402780@qq.com>
2026-09-19 22:50:57 +0800

    add quality inspection snapshot and diff runtime

---
+++ src/Asun.Domain.Quality/QualityInspectionDiffRuntime.cs
@@ -0,0 +1,61 @@
+namespace Asun.Domain.Quality;
+
+public static class QualityInspectionDiffRuntime
+{
+    public static QualityInspectionDiff Diff(
+        QualityInspectionSnapshot previous,
+        QualityInspectionSnapshot current)
+    {
+        ArgumentNullException.ThrowIfNull(previous);
+        ArgumentNullException.ThrowIfNull(current);
+
+        var previousFindings = previous.Findings.Findings
+            .ToDictionary(finding => finding.Id);
+        var currentFindings = current.Findings.Findings
+            .ToDictionary(finding => finding.Id);
+
+        var addedFindings = currentFindings.Keys
+            .Except(previousFindings.Keys)
+            .OrderBy(id => id.Value, StringComparer.Ordinal)
+            .ToArray();
+
+        var removedFindings = previousFindings.Keys
+            .Except(currentFindings.Keys)
+            .OrderBy(id => id.Value, StringComparer.Ordinal)
+            .ToArray();
+
+        var changedFindings = currentFindings.Keys
+            .Intersect(previousFindings.Keys)
+            .Where(id => previousFindings[id] != currentFindings[id])
+            .OrderBy(id => id.Value, StringComparer.Ordinal)
+            .ToArray();
+
+        var previousEvidence = previous.Evidence.Links
+            .Select(link => (link.FindingId, link.EvidenceKey))
+            .ToHashSet();
+        var currentEvidence = current.Evidence.Links
+            .Select(link => (link.FindingId, link.EvidenceKey))
+            .ToHashSet();
+
+        var addedEvidence = currentEvidence
+            .Except(previousEvidence)
+            .Select(pair => pair.EvidenceKey)
+            .Distinct()
+            .OrderBy(key => key.Value, StringComparer.Ordinal)
+            .ToArray();
+
+        var removedEvidence = previousEvidence
+            .Except(currentEvidence)
+            .Select(pair => pair.EvidenceKey)
+            .Distinct()
+            .OrderBy(key => key.Value, StringComparer.Ordinal)
+            .ToArray();
+
+        return new QualityInspectionDiff(
+            addedFindings,
+            removedFindings,
+            changedFindings,
+            addedEvidence,
+            removedEvidence);
+    }
+}
