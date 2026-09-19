f1230090235c20caea5c549208b9f4a00f3903b3
asun01 <254402780@qq.com>
2026-09-19 15:31:22 +0800

    Wire render surface transaction smoke

--- tests/Asun.UI.Viewports.Smoke/Program.cs
+++ tests/Asun.UI.Viewports.Smoke/Program.cs
@@ -250,6 +250,7 @@ await ViewportPresentationFacadeSmoke.RunAsync(Assert);
 
 await ViewportDeliveryAndBackpressureSmoke.RunAsync(Assert);
 await ViewportContinuousAndDeferredSmoke.RunAsync(Assert);
+await ViewportRenderSurfaceSmoke.RunAsync(Assert);
 
 await ViewportPresentationLifecycleSmoke.RunAsync(Assert);
 
