325fedf5102399ff839de520b27ce7513070be5c
asun01 <254402780@qq.com>
2026-09-19 22:43:50 +0800

    add PCB domain primitive

---
+++ src/Asun.Domain.Pcb/PcbFeatureKind.cs
@@ -0,0 +1,14 @@
+namespace Asun.Domain.Pcb;
+
+public enum PcbFeatureKind
+{
+    Unknown = 0,
+    Pad = 1,
+    Via = 2,
+    Hole = 3,
+    Trace = 4,
+    Fiducial = 5,
+    ComponentReference = 6,
+    BoardOutline = 7,
+    Region = 8
+}
