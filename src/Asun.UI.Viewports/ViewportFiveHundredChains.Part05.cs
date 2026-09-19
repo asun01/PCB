13cfeb26dcaf6a7f7e6cc575c91e709675b359c9
asun01 <254402780@qq.com>
2026-09-19 13:38:49 +0800

    feat: add fifty executable bulk chains - part 5

---
+++ src/Asun.UI.Viewports/ViewportFiveHundredChains.Part05.cs
@@ -0,0 +1,154 @@
+namespace Asun.UI.Viewports;
+
+public static partial class ViewportFiveHundredChains
+{
+    public static BulkChainOutcome Chain201(BulkChainContext context) =>
+        BulkChainKernel.Execute(201, "Overlay", context, 5, 2.0d);
+
+    public static BulkChainOutcome Chain202(BulkChainContext context) =>
+        BulkChainKernel.Execute(202, "Overlay", context, 6, 2.5d);
+
+    public static BulkChainOutcome Chain203(BulkChainContext context) =>
+        BulkChainKernel.Execute(203, "Overlay", context, 7, 3.0d);
+
+    public static BulkChainOutcome Chain204(BulkChainContext context) =>
+        BulkChainKernel.Execute(204, "Overlay", context, 0, 3.5d);
+
+    public static BulkChainOutcome Chain205(BulkChainContext context) =>
+        BulkChainKernel.Execute(205, "Overlay", context, 1, 4.0d);
+
+    public static BulkChainOutcome Chain206(BulkChainContext context) =>
+        BulkChainKernel.Execute(206, "Overlay", context, 2, 4.5d);
+
+    public static BulkChainOutcome Chain207(BulkChainContext context) =>
+        BulkChainKernel.Execute(207, "Overlay", context, 3, 0.5d);
+
+    public static BulkChainOutcome Chain208(BulkChainContext context) =>
+        BulkChainKernel.Execute(208, "Overlay", context, 4, 1.0d);
+
+    public static BulkChainOutcome Chain209(BulkChainContext context) =>
+        BulkChainKernel.Execute(209, "Overlay", context, 5, 1.5d);
+
+    public static BulkChainOutcome Chain210(BulkChainContext context) =>
+        BulkChainKernel.Execute(210, "Overlay", context, 6, 2.0d);
+
+    public static BulkChainOutcome Chain211(BulkChainContext context) =>
+        BulkChainKernel.Execute(211, "Overlay", context, 7, 2.5d);
+
+    public static BulkChainOutcome Chain212(BulkChainContext context) =>
+        BulkChainKernel.Execute(212, "Overlay", context, 0, 3.0d);
+
+    public static BulkChainOutcome Chain213(BulkChainContext context) =>
+        BulkChainKernel.Execute(213, "Overlay", context, 1, 3.5d);
+
+    public static BulkChainOutcome Chain214(BulkChainContext context) =>
+        BulkChainKernel.Execute(214, "Overlay", context, 2, 4.0d);
+
+    public static BulkChainOutcome Chain215(BulkChainContext context) =>
+        BulkChainKernel.Execute(215, "Overlay", context, 3, 4.5d);
+
+    public static BulkChainOutcome Chain216(BulkChainContext context) =>
+        BulkChainKernel.Execute(216, "Overlay", context, 4, 0.5d);
+
+    public static BulkChainOutcome Chain217(BulkChainContext context) =>
+        BulkChainKernel.Execute(217, "Overlay", context, 5, 1.0d);
+
+    public static BulkChainOutcome Chain218(BulkChainContext context) =>
+        BulkChainKernel.Execute(218, "Overlay", context, 6, 1.5d);
+
+    public static BulkChainOutcome Chain219(BulkChainContext context) =>
+        BulkChainKernel.Execute(219, "Overlay", context, 7, 2.0d);
+
+    public static BulkChainOutcome Chain220(BulkChainContext context) =>
+        BulkChainKernel.Execute(220, "Overlay", context, 0, 2.5d);
+
+    public static BulkChainOutcome Chain221(BulkChainContext context) =>
+        BulkChainKernel.Execute(221, "Overlay", context, 1, 3.0d);
+
+    public static BulkChainOutcome Chain222(BulkChainContext context) =>
+        BulkChainKernel.Execute(222, "Overlay", context, 2, 3.5d);
+
+    public static BulkChainOutcome Chain223(BulkChainContext context) =>
+        BulkChainKernel.Execute(223, "Overlay", context, 3, 4.0d);
+
+    public static BulkChainOutcome Chain224(BulkChainContext context) =>
+        BulkChainKernel.Execute(224, "Overlay", context, 4, 4.5d);
+
+    public static BulkChainOutcome Chain225(BulkChainContext context) =>
+        BulkChainKernel.Execute(225, "Overlay", context, 5, 0.5d);
+
+    public static BulkChainOutcome Chain226(BulkChainContext context) =>
+        BulkChainKernel.Execute(226, "Overlay", context, 6, 1.0d);
+
+    public static BulkChainOutcome Chain227(BulkChainContext context) =>
+        BulkChainKernel.Execute(227, "Overlay", context, 7, 1.5d);
+
+    public static BulkChainOutcome Chain228(BulkChainContext context) =>
+        BulkChainKernel.Execute(228, "Overlay", context, 0, 2.0d);
+
+    public static BulkChainOutcome Chain229(BulkChainContext context) =>
+        BulkChainKernel.Execute(229, "Overlay", context, 1, 2.5d);
+
+    public static BulkChainOutcome Chain230(BulkChainContext context) =>
+        BulkChainKernel.Execute(230, "Overlay", context, 2, 3.0d);
+
+    public static BulkChainOutcome Chain231(BulkChainContext context) =>
+        BulkChainKernel.Execute(231, "Overlay", context, 3, 3.5d);
+
+    public static BulkChainOutcome Chain232(BulkChainContext context) =>
+        BulkChainKernel.Execute(232, "Overlay", context, 4, 4.0d);
+
+    public static BulkChainOutcome Chain233(BulkChainContext context) =>
+        BulkChainKernel.Execute(233, "Overlay", context, 5, 4.5d);
+
+    public static BulkChainOutcome Chain234(BulkChainContext context) =>
+        BulkChainKernel.Execute(234, "Overlay", context, 6, 0.5d);
+
+    public static BulkChainOutcome Chain235(BulkChainContext context) =>
+        BulkChainKernel.Execute(235, "Overlay", context, 7, 1.0d);
+
+    public static BulkChainOutcome Chain236(BulkChainContext context) =>
+        BulkChainKernel.Execute(236, "Overlay", context, 0, 1.5d);
+
+    public static BulkChainOutcome Chain237(BulkChainContext context) =>
+        BulkChainKernel.Execute(237, "Overlay", context, 1, 2.0d);
+
+    public static BulkChainOutcome Chain238(BulkChainContext context) =>
+        BulkChainKernel.Execute(238, "Overlay", context, 2, 2.5d);
+
+    public static BulkChainOutcome Chain239(BulkChainContext context) =>
+        BulkChainKernel.Execute(239, "Overlay", context, 3, 3.0d);
+
+    public static BulkChainOutcome Chain240(BulkChainContext context) =>
+        BulkChainKernel.Execute(240, "Overlay", context, 4, 3.5d);
+
+    public static BulkChainOutcome Chain241(BulkChainContext context) =>
+        BulkChainKernel.Execute(241, "Overlay", context, 5, 4.0d);
+
+    public static BulkChainOutcome Chain242(BulkChainContext context) =>
+        BulkChainKernel.Execute(242, "Overlay", context, 6, 4.5d);
+
+    public static BulkChainOutcome Chain243(BulkChainContext context) =>
+        BulkChainKernel.Execute(243, "Overlay", context, 7, 0.5d);
+
+    public static BulkChainOutcome Chain244(BulkChainContext context) =>
+        BulkChainKernel.Execute(244, "Overlay", context, 0, 1.0d);
+
+    public static BulkChainOutcome Chain245(BulkChainContext context) =>
+        BulkChainKernel.Execute(245, "Overlay", context, 1, 1.5d);
+
+    public static BulkChainOutcome Chain246(BulkChainContext context) =>
+        BulkChainKernel.Execute(246, "Overlay", context, 2, 2.0d);
+
+    public static BulkChainOutcome Chain247(BulkChainContext context) =>
+        BulkChainKernel.Execute(247, "Overlay", context, 3, 2.5d);
+
+    public static BulkChainOutcome Chain248(BulkChainContext context) =>
+        BulkChainKernel.Execute(248, "Overlay", context, 4, 3.0d);
+
+    public static BulkChainOutcome Chain249(BulkChainContext context) =>
+        BulkChainKernel.Execute(249, "Overlay", context, 5, 3.5d);
+
+    public static BulkChainOutcome Chain250(BulkChainContext context) =>
+        BulkChainKernel.Execute(250, "Overlay", context, 6, 4.0d);
+}
