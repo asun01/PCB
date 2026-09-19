4155ec659f80cf74fca79d1910b35ff1dbf843b6
asun01 <254402780@qq.com>
2026-09-19 14:31:24 +0800

    chore(input): remove unused composite gesture scaffold

--- src/Asun.UI.Viewports/ViewportCompositeGestureRuntime.cs
+++
@@ -1,9 +0,0 @@
-using System.Numerics;
-
-namespace Asun.UI.Viewports;
-
-public sealed class ViewportCompositeGestureRuntime
-{
-    private readonly object _sync = new();
-    private readonly ViewportCompositeRuntime<object> _unused;
-}
