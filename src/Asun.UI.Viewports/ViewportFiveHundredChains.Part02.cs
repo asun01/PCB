d79be8a92fbe0a70d0301a76add8d42b452749ad
asun01 <254402780@qq.com>
2026-09-19 13:38:43 +0800

    feat: add fifty executable bulk chains - part 2

---
+++ src/Asun.UI.Viewports/ViewportFiveHundredChains.Part02.cs
@@ -0,0 +1,154 @@
+namespace Asun.UI.Viewports;
+
+public static partial class ViewportFiveHundredChains
+{
+    public static BulkChainOutcome Chain051(BulkChainContext context) =>
+        BulkChainKernel.Execute(51, "RoiGeometry", context, 4, 3.5d);
+
+    public static BulkChainOutcome Chain052(BulkChainContext context) =>
+        BulkChainKernel.Execute(52, "RoiGeometry", context, 5, 4.0d);
+
+    public static BulkChainOutcome Chain053(BulkChainContext context) =>
+        BulkChainKernel.Execute(53, "RoiGeometry", context, 6, 4.5d);
+
+    public static BulkChainOutcome Chain054(BulkChainContext context) =>
+        BulkChainKernel.Execute(54, "RoiGeometry", context, 7, 0.5d);
+
+    public static BulkChainOutcome Chain055(BulkChainContext context) =>
+        BulkChainKernel.Execute(55, "RoiGeometry", context, 0, 1.0d);
+
+    public static BulkChainOutcome Chain056(BulkChainContext context) =>
+        BulkChainKernel.Execute(56, "RoiGeometry", context, 1, 1.5d);
+
+    public static BulkChainOutcome Chain057(BulkChainContext context) =>
+        BulkChainKernel.Execute(57, "RoiGeometry", context, 2, 2.0d);
+
+    public static BulkChainOutcome Chain058(BulkChainContext context) =>
+        BulkChainKernel.Execute(58, "RoiGeometry", context, 3, 2.5d);
+
+    public static BulkChainOutcome Chain059(BulkChainContext context) =>
+        BulkChainKernel.Execute(59, "RoiGeometry", context, 4, 3.0d);
+
+    public static BulkChainOutcome Chain060(BulkChainContext context) =>
+        BulkChainKernel.Execute(60, "RoiGeometry", context, 5, 3.5d);
+
+    public static BulkChainOutcome Chain061(BulkChainContext context) =>
+        BulkChainKernel.Execute(61, "RoiGeometry", context, 6, 4.0d);
+
+    public static BulkChainOutcome Chain062(BulkChainContext context) =>
+        BulkChainKernel.Execute(62, "RoiGeometry", context, 7, 4.5d);
+
+    public static BulkChainOutcome Chain063(BulkChainContext context) =>
+        BulkChainKernel.Execute(63, "RoiGeometry", context, 0, 0.5d);
+
+    public static BulkChainOutcome Chain064(BulkChainContext context) =>
+        BulkChainKernel.Execute(64, "RoiGeometry", context, 1, 1.0d);
+
+    public static BulkChainOutcome Chain065(BulkChainContext context) =>
+        BulkChainKernel.Execute(65, "RoiGeometry", context, 2, 1.5d);
+
+    public static BulkChainOutcome Chain066(BulkChainContext context) =>
+        BulkChainKernel.Execute(66, "RoiGeometry", context, 3, 2.0d);
+
+    public static BulkChainOutcome Chain067(BulkChainContext context) =>
+        BulkChainKernel.Execute(67, "RoiGeometry", context, 4, 2.5d);
+
+    public static BulkChainOutcome Chain068(BulkChainContext context) =>
+        BulkChainKernel.Execute(68, "RoiGeometry", context, 5, 3.0d);
+
+    public static BulkChainOutcome Chain069(BulkChainContext context) =>
+        BulkChainKernel.Execute(69, "RoiGeometry", context, 6, 3.5d);
+
+    public static BulkChainOutcome Chain070(BulkChainContext context) =>
+        BulkChainKernel.Execute(70, "RoiGeometry", context, 7, 4.0d);
+
+    public static BulkChainOutcome Chain071(BulkChainContext context) =>
+        BulkChainKernel.Execute(71, "RoiGeometry", context, 0, 4.5d);
+
+    public static BulkChainOutcome Chain072(BulkChainContext context) =>
+        BulkChainKernel.Execute(72, "RoiGeometry", context, 1, 0.5d);
+
+    public static BulkChainOutcome Chain073(BulkChainContext context) =>
+        BulkChainKernel.Execute(73, "RoiGeometry", context, 2, 1.0d);
+
+    public static BulkChainOutcome Chain074(BulkChainContext context) =>
+        BulkChainKernel.Execute(74, "RoiGeometry", context, 3, 1.5d);
+
+    public static BulkChainOutcome Chain075(BulkChainContext context) =>
+        BulkChainKernel.Execute(75, "RoiGeometry", context, 4, 2.0d);
+
+    public static BulkChainOutcome Chain076(BulkChainContext context) =>
+        BulkChainKernel.Execute(76, "RoiGeometry", context, 5, 2.5d);
+
+    public static BulkChainOutcome Chain077(BulkChainContext context) =>
+        BulkChainKernel.Execute(77, "RoiGeometry", context, 6, 3.0d);
+
+    public static BulkChainOutcome Chain078(BulkChainContext context) =>
+        BulkChainKernel.Execute(78, "RoiGeometry", context, 7, 3.5d);
+
+    public static BulkChainOutcome Chain079(BulkChainContext context) =>
+        BulkChainKernel.Execute(79, "RoiGeometry", context, 0, 4.0d);
+
+    public static BulkChainOutcome Chain080(BulkChainContext context) =>
+        BulkChainKernel.Execute(80, "RoiGeometry", context, 1, 4.5d);
+
+    public static BulkChainOutcome Chain081(BulkChainContext context) =>
+        BulkChainKernel.Execute(81, "RoiGeometry", context, 2, 0.5d);
+
+    public static BulkChainOutcome Chain082(BulkChainContext context) =>
+        BulkChainKernel.Execute(82, "RoiGeometry", context, 3, 1.0d);
+
+    public static BulkChainOutcome Chain083(BulkChainContext context) =>
+        BulkChainKernel.Execute(83, "RoiGeometry", context, 4, 1.5d);
+
+    public static BulkChainOutcome Chain084(BulkChainContext context) =>
+        BulkChainKernel.Execute(84, "RoiGeometry", context, 5, 2.0d);
+
+    public static BulkChainOutcome Chain085(BulkChainContext context) =>
+        BulkChainKernel.Execute(85, "RoiGeometry", context, 6, 2.5d);
+
+    public static BulkChainOutcome Chain086(BulkChainContext context) =>
+        BulkChainKernel.Execute(86, "RoiGeometry", context, 7, 3.0d);
+
+    public static BulkChainOutcome Chain087(BulkChainContext context) =>
+        BulkChainKernel.Execute(87, "RoiGeometry", context, 0, 3.5d);
+
+    public static BulkChainOutcome Chain088(BulkChainContext context) =>
+        BulkChainKernel.Execute(88, "RoiGeometry", context, 1, 4.0d);
+
+    public static BulkChainOutcome Chain089(BulkChainContext context) =>
+        BulkChainKernel.Execute(89, "RoiGeometry", context, 2, 4.5d);
+
+    public static BulkChainOutcome Chain090(BulkChainContext context) =>
+        BulkChainKernel.Execute(90, "RoiGeometry", context, 3, 0.5d);
+
+    public static BulkChainOutcome Chain091(BulkChainContext context) =>
+        BulkChainKernel.Execute(91, "RoiGeometry", context, 4, 1.0d);
+
+    public static BulkChainOutcome Chain092(BulkChainContext context) =>
+        BulkChainKernel.Execute(92, "RoiGeometry", context, 5, 1.5d);
+
+    public static BulkChainOutcome Chain093(BulkChainContext context) =>
+        BulkChainKernel.Execute(93, "RoiGeometry", context, 6, 2.0d);
+
+    public static BulkChainOutcome Chain094(BulkChainContext context) =>
+        BulkChainKernel.Execute(94, "RoiGeometry", context, 7, 2.5d);
+
+    public static BulkChainOutcome Chain095(BulkChainContext context) =>
+        BulkChainKernel.Execute(95, "RoiGeometry", context, 0, 3.0d);
+
+    public static BulkChainOutcome Chain096(BulkChainContext context) =>
+        BulkChainKernel.Execute(96, "RoiGeometry", context, 1, 3.5d);
+
+    public static BulkChainOutcome Chain097(BulkChainContext context) =>
+        BulkChainKernel.Execute(97, "RoiGeometry", context, 2, 4.0d);
+
+    public static BulkChainOutcome Chain098(BulkChainContext context) =>
+        BulkChainKernel.Execute(98, "RoiGeometry", context, 3, 4.5d);
+
+    public static BulkChainOutcome Chain099(BulkChainContext context) =>
+        BulkChainKernel.Execute(99, "RoiGeometry", context, 4, 0.5d);
+
+    public static BulkChainOutcome Chain100(BulkChainContext context) =>
+        BulkChainKernel.Execute(100, "RoiGeometry", context, 5, 1.0d);
+}
