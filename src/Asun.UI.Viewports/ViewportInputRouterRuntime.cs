e76f492698fc21631d323e8dea5e1bfcab09f588
asun01 <254402780@qq.com>
2026-09-19 20:21:15 +0800

    phase1: fix input router capture ownership

--- src/Asun.UI.Viewports/ViewportInputRouterRuntime.cs
+++ src/Asun.UI.Viewports/ViewportInputRouterRuntime.cs
@@ -6,6 +6,7 @@ public sealed class ViewportInputRouterRuntime
 {
     private readonly ViewportGestureRuntime _gestures;
     private readonly ViewportInputCaptureRuntime _capture;
+    private ViewportInputOwner _capturedOwner;
 
     public ViewportInputRouterRuntime(
         ViewportGestureRuntime gestures,
@@ -15,30 +16,77 @@ public sealed class ViewportInputRouterRuntime
         ArgumentNullException.ThrowIfNull(capture);
         _gestures = gestures;
         _capture = capture;
+        _capturedOwner = ViewportInputOwner.None;
     }
 
-    public ViewportGestureEvent PointerDown(Vector2 point, ViewportMouseButton button = ViewportMouseButton.Left)
+    public ViewportGestureEvent PointerDown(
+        Vector2 point,
+        ViewportMouseButton button = ViewportMouseButton.Left)
     {
-        var owner = button == ViewportMouseButton.Middle ? ViewportInputOwner.Pan : ViewportInputOwner.Roi;
-        if (!_capture.TryCapture(owner))
+        var owner = button switch
+        {
+            ViewportMouseButton.Middle => ViewportInputOwner.Pan,
+            ViewportMouseButton.Left => ViewportInputOwner.Roi,
+            _ => ViewportInputOwner.None
+        };
+
+        if (owner == ViewportInputOwner.None ||
+            !_capture.TryCapture(owner))
             return default;
-        return _gestures.PointerDown(point, button);
+
+        try
+        {
+            var result = _gestures.PointerDown(point, button);
+
+            if (!result.Kind.Equals(ViewportGestureKind.Idle))
+            {
+                _capturedOwner = owner;
+                return result;
+            }
+
+            _capture.Release(owner);
+            return result;
+        }
+        catch
+        {
+            _capture.Release(owner);
+            throw;
+        }
     }
 
-    public ViewportGestureEvent PointerMove(Vector2 point) =>
-        _gestures.PointerMove(point);
+    public ViewportGestureEvent PointerMove(Vector2 point)
+    {
+        if (_capturedOwner == ViewportInputOwner.None)
+            return _gestures.PointerMove(point);
+
+        return _gestures.PointerMove(point);
+    }
 
     public ViewportGestureEvent PointerUp(Vector2 point)
     {
         var result = _gestures.PointerUp(point);
-        _capture.ForceRelease();
+
+        if (_capturedOwner != ViewportInputOwner.None)
+        {
+            _capture.Release(_capturedOwner);
+            _capturedOwner = ViewportInputOwner.None;
+        }
+
         return result;
     }
 
     public bool Escape(Vector2 point)
     {
         var result = _gestures.KeyDown(ViewportKey.Escape, point);
-        _capture.ForceRelease();
+
+        if (_capturedOwner != ViewportInputOwner.None)
+        {
+            _capture.Release(_capturedOwner);
+            _capturedOwner = ViewportInputOwner.None;
+        }
+
         return result;
     }
+
+    public ViewportInputOwner CapturedOwner => _capturedOwner;
 }
