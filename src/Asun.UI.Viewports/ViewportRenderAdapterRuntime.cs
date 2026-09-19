2d8bbf86636f02a61ac590c2e3e3dde27f8e5e4a
asun01 <254402780@qq.com>
2026-09-19 14:55:26 +0800

    fix(viewport): correct tile delivery unit counter

--- src/Asun.UI.Viewports/ViewportRenderAdapterRuntime.cs
+++ src/Asun.UI.Viewports/ViewportRenderAdapterRuntime.cs
@@ -108,7 +108,7 @@ public static class ViewportRenderAdapterRuntime
                         cancellationToken)
                     .ConfigureAwait(false);
 
-                renderedTiles++;
+                renderedUnits++;
                 continue;
             }
 
