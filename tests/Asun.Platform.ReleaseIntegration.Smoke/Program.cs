565ef9c9f8c11c55e7b5890e4f4e1b47154c0ee8
asun01 <254402780@qq.com>
2026-09-20 14:33:46 +0800

    test: register production release replay descriptor smokes

--- tests/Asun.Platform.ReleaseIntegration.Smoke/Program.cs
+++ tests/Asun.Platform.ReleaseIntegration.Smoke/Program.cs
@@ -7,6 +7,11 @@ void Check(bool condition,string message)
 }
 
 await ProductionEvidenceReleaseProjectionHundredStageSmoke.RunAsync(Check);
+await ProductionReleaseReplayDescriptor1HundredStageSmoke.RunAsync(Check);
+await ProductionReleaseReplayDescriptor2HundredStageSmoke.RunAsync(Check);
+await ProductionReleaseReplayDescriptor3HundredStageSmoke.RunAsync(Check);
+await ProductionReleaseReplayDescriptor4HundredStageSmoke.RunAsync(Check);
+await ProductionReleaseReplayDescriptor5HundredStageSmoke.RunAsync(Check);
 
 if(failures.Count>0)
 {
