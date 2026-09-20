d4692a6d158b6966a1a0408b33687caed7dfd409
asun01 <254402780@qq.com>
2026-09-20 20:19:39 +0800

    feat(shell): add WPF ROI pointer input adapter

---
+++ src/Asun.App.Shell/Bootstrap/WpfRoiInputAdapter.cs
@@ -0,0 +1,81 @@
+using System.Numerics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Asun.UI.Viewports;
+
+namespace Asun.App.Shell.Bootstrap;
+
+public sealed class WpfRoiInputAdapter
+{
+    private readonly ClientIntegration.ClientRoiInteractionWorkspace _workspace;
+    private readonly FrameworkElement _host;
+
+    public WpfRoiInputAdapter(
+        ClientIntegration.ClientRoiInteractionWorkspace workspace,
+        FrameworkElement host)
+    {
+        ArgumentNullException.ThrowIfNull(workspace);
+        ArgumentNullException.ThrowIfNull(host);
+        _workspace=workspace;
+        _host=host;
+    }
+
+    public bool MouseDown(MouseButtonEventArgs args)
+    {
+        ArgumentNullException.ThrowIfNull(args);
+        _host.Focus();
+
+        var point=ToVector(args.GetPosition(_host));
+        var button=args.ChangedButton==MouseButton.Right
+            ? ViewportMouseButton.Right
+            : ViewportMouseButton.Left;
+
+        return _workspace.Submit(
+            ViewportInputEventKind.PointerDown,
+            point,
+            button:button);
+    }
+
+    public bool MouseMove(MouseEventArgs args)
+    {
+        ArgumentNullException.ThrowIfNull(args);
+        var point=ToVector(args.GetPosition(_host));
+        return _workspace.Submit(
+            ViewportInputEventKind.PointerMove,
+            point);
+    }
+
+    public bool MouseUp(MouseButtonEventArgs args)
+    {
+        ArgumentNullException.ThrowIfNull(args);
+        var point=ToVector(args.GetPosition(_host));
+        return _workspace.Submit(
+            ViewportInputEventKind.PointerUp,
+            point);
+    }
+
+    public bool MouseWheel(MouseWheelEventArgs args)
+    {
+        ArgumentNullException.ThrowIfNull(args);
+        var point=ToVector(args.GetPosition(_host));
+        return _workspace.Submit(
+            ViewportInputEventKind.Wheel,
+            point,
+            args.Delta);
+    }
+
+    public bool Escape()
+    {
+        var point=new Vector2(
+            (float)(_host.ActualWidth/2d),
+            (float)(_host.ActualHeight/2d));
+
+        return _workspace.Submit(
+            ViewportInputEventKind.Escape,
+            point);
+    }
+
+    private static Vector2 ToVector(Point point)=>
+        new((float)point.X,(float)point.Y);
+}
