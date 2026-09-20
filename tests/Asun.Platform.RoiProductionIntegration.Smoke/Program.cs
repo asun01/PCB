68b67c91cd9979100365353690ff6d02bd5c838a
asun01 <254402780@qq.com>
2026-09-20 19:26:49 +0800

    test(roi): register ROI measurement quality smokes

--- tests/Asun.Platform.RoiProductionIntegration.Smoke/Program.cs
+++ tests/Asun.Platform.RoiProductionIntegration.Smoke/Program.cs
@@ -21,6 +21,12 @@ await ProductionRoiRenderEvidenceReplayContext2HundredStageSmoke.RunAsync(Check)
 await ProductionRoiRenderEvidenceReplayContext3HundredStageSmoke.RunAsync(Check);
 await ProductionRoiRenderEvidenceReplayContext4HundredStageSmoke.RunAsync(Check);
 await ProductionRoiRenderEvidenceReplayContext5HundredStageSmoke.RunAsync(Check);
+await ProductionRoiMeasurementQualityContext1HundredStageSmoke.RunAsync(Check);
+await ProductionRoiMeasurementQualityContext2HundredStageSmoke.RunAsync(Check);
+await ProductionRoiMeasurementQualityContext3HundredStageSmoke.RunAsync(Check);
+await ProductionRoiMeasurementQualityContext4HundredStageSmoke.RunAsync(Check);
+await ProductionRoiMeasurementQualityContext5HundredStageSmoke.RunAsync(Check);
+
 
 
 
