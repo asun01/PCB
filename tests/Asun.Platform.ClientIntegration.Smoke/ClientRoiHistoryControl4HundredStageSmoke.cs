dca7a09d7d3b37b634fc18acf0667fc5e32473d0
asun01 <254402780@qq.com>
2026-09-20 20:31:15 +0800

    test(client): add ClientRoiHistoryControl4HundredStageSmoke

---
+++ tests/Asun.Platform.ClientIntegration.Smoke/ClientRoiHistoryControl4HundredStageSmoke.cs
@@ -0,0 +1,127 @@
+using System.Numerics;
+using Asun.Platform.ClientIntegration;
+using Asun.UI.Viewports;
+
+namespace Asun.Platform.ClientIntegration.Smoke;
+
+public static class ClientRoiHistoryControl4HundredStageSmoke
+{
+    public static async Task RunAsync(Action<bool,string> Check)
+    {
+        var round=0;
+        for(var i0=0;i0<10;i0++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i1=0;i1<10;i1++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i2=0;i2<10;i2++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i3=0;i3<10;i3++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i4=0;i4<10;i4++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i5=0;i5<10;i5++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i6=0;i6<10;i6++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i7=0;i7<10;i7++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i8=0;i8<10;i8++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        for(var i9=0;i9<10;i9++)
+        {
+            round++;
+                using var client=new ClientInspectionWorkspace(new Vector2(100,100),new Vector2(400,400));
+                client.Load(ClientWorkspaceSmokeFixture.CreateDefinition());
+                await client.ExecuteAsync(
+                    new Asun.Device.Impl.SimulatedFrameSource(8,8),
+                    ClientWorkspaceSmokeFixture.CreateReleaseManifest());
+                Check(!client.UndoRoi() && !client.RedoRoi(),
+                      "Undo and Redo should be safe no-ops when there is no ROI history");
+        }
+        if(round==100)
+            return;
+
+        throw new InvalidOperationException("client ROI history smoke must execute exactly 100 rounds");
+    }
+}
