cbf7d2d22695e9ff505c57de6007e19407150f49
asun01 <254402780@qq.com>
2026-09-20 20:56:05 +0800

    test(client): register result presentation matrices

--- tests/Asun.Platform.ClientIntegration.Smoke/Program.cs
+++ tests/Asun.Platform.ClientIntegration.Smoke/Program.cs
@@ -102,6 +102,12 @@ await ClientWorkspaceLifecycle3HundredStageSmoke.RunAsync(Check);
 await ClientWorkspaceLifecycle4HundredStageSmoke.RunAsync(Check);
 await ClientWorkspaceLifecycle5HundredStageSmoke.RunAsync(Check);
 
+await ClientResultsPresentation1HundredStageSmoke.RunAsync(Check);
+await ClientResultsPresentation2HundredStageSmoke.RunAsync(Check);
+await ClientResultsPresentation3HundredStageSmoke.RunAsync(Check);
+await ClientResultsPresentation4HundredStageSmoke.RunAsync(Check);
+await ClientResultsPresentation5HundredStageSmoke.RunAsync(Check);
+
 if(failures.Count>0)
 {
     foreach(var failure in failures)
