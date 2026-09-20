ffcf7c21bf95a6f86b217b835620c88dcb9d535e
asun01 <254402780@qq.com>
2026-09-20 19:23:23 +0800

    test(roi): add ProductionRoiInteractionContext5HundredStageSmoke

---
+++ tests/Asun.Platform.RoiProductionIntegration.Smoke/ProductionRoiInteractionContext5HundredStageSmoke.cs
@@ -0,0 +1,132 @@
+using Asun.Platform.RoiProductionIntegration;
+
+namespace Asun.Platform.RoiProductionIntegration.Smoke;
+
+public static class ProductionRoiInteractionContext5HundredStageSmoke
+{
+    public static Task RunAsync(Action<bool,string> Check)
+    {
+        var round=0;
+        for(var group0=0;group0<10;group0++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group0<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group1=0;group1<10;group1++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group1<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group2=0;group2<10;group2++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group2<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group3=0;group3<10;group3++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group3<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group4=0;group4<10;group4++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group4<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group5=0;group5<10;group5++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group5<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group6=0;group6<10;group6++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group6<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group7=0;group7<10;group7++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group7<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group8=0;group8<10;group8++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group8<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        for(var group9=0;group9<10;group9++)
+        {
+            round++;
+                var left=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var right=ProductionRoiInteractionContextSmokeFixtures.Create();
+                var a=ProductionRoiInteractionContextRuntime.Create(left.Production,left.RoiSnapshot);
+                var b=ProductionRoiInteractionContextRuntime.Create(right.Production,right.RoiSnapshot);
+                Check(ProductionRoiInteractionContextRuntime.IsEquivalent(a,b) &&
+                      a.BindingFingerprint==b.BindingFingerprint &&
+                      (group9<9 || round==100),
+                      "equivalent production ROI contexts must converge deterministically");
+        }
+        return Task.CompletedTask;
+    }
+}
